95e7b1d1030e2d120893df7a8183180f9e365109
Sendra8 <sendra.adria@gmail.com>
2023-11-21 12:33:57 +0100

    Initial commit

---
+++ Dot2Coma.py
@@ -0,0 +1,24 @@
+import sys
+
+def replace_dots_with_commas(csv_file):
+    # Leer el contenido del archivo como texto
+    with open(csv_file, 'r') as file:
+        content = file.read()
+
+    # Reemplazar puntos por comas
+    content = content.replace('.', ',')
+
+    # Construir el nombre del archivo de salida
+    output_file = csv_file.rsplit('.', 1)[0] + '_wComa.csv'
+
+    # Guardar el contenido modificado en un nuevo archivo
+    with open(output_file, 'w') as file:
+        file.write(content)
+
+    print(f"Archivo convertido y guardado como: {output_file}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso: python script.py <archivo.csv>")
+    else:
+        replace_dots_with_commas(sys.argv[1])
